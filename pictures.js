083b62e822e790b0e745f47fcf9c7c789c451163
Emanuel <cascoemanuel@gmail.com>
2017-04-10 19:54:54 -0300

    Creation of GET /picture endpoint

--- pictures.js
+++ pictures.js
@@ -1,11 +1,27 @@
 'use strict'
 
+import { send } from 'micro'
 import HttpHash from 'http-hash'
+import DB from 'emagram-db'
+import DbStub from './test/stub/db'
+import config from './config'
+
+const env = process.env.NODE_ENV || 'production'
+
+let db = new DB(config.db)
+
+if (env === 'test') {
+  db = new DbStub()
+}
 
 const hash = HttpHash()
 
 hash.set('GET /:id', async function getPicture (req, res, params) {
-  send(res, 200, params)
+  let id = params.id
+  await db.connect()
+  let image = db.getImage(id)
+  await db.disconnect()
+  send(res, 200, image)
 })
 
 export default async function main (req, res) {
@@ -15,7 +31,7 @@ export default async function main (req, res) {
 
   if (match.handler) {
     try {
-      await match.hanlder(req, res, match.params)
+      await match.handler(req, res, match.params)
     } catch (e) {
       send(res, 500, { error: e.message })
     }
---
+++ test/fixtures/index.js
@@ -0,0 +1,12 @@
+export default {
+  getImage () {
+    return {
+      id: 'c0ff1499-6a84-4588-a388-493d65ef5eb8',
+      publicId: '1aTCPnhHEWyDqCaNkVWXte',
+      userId: 'emagram'
+      src: 'http://emagram.test/1aTCPnhHEWyDqCaNkVWXte.jpg',
+      description: "#awesome",
+      createdAr: new Date().toString()
+    }
+  }
+}
